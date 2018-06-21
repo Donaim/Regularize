80a0a2bfce5e7dfcd401452d7c31d6e268bb0837
Donaim <3Donaim@gmail.com>
2018-06-21 22:36:56 +0200

    improved parsing

--- cli.py
+++ cli.py
@@ -1,8 +1,9 @@
+from inspect import signature
+
 import window
 import file_manager
 import cmap
 import updater
-
 
 class Parser:
 
@@ -20,9 +21,8 @@ class Parser:
         print('saving...')
         raise NotImplementedError()
         print('saved')
-    def change_width(self):
-        args = self._parse_arguments(1)
-        new_width = int(args[0])
+    def change_width(self, new_width):
+        new_width = int(new_width)
         cmap.CELL_W = new_width
         cmap.CELL_H = new_width
         window.resize()
@@ -30,7 +30,7 @@ class Parser:
 
     def _parse_arguments(self, expected_num: int) -> list:
         sp = self._command.split()
-        if len(sp) < expected_num + 1:
+        if len(sp) != 1 + expected_num:
             print('expected {} arguments but {} were given'.format(expected_num, len(sp) - 1))
             return None
         return sp[1:]
@@ -48,7 +48,11 @@ class Parser:
                 self._forse_exit = True
                 return
             elif func in funcs:
-                funcs[func]()
+                f = funcs[func]
+                need_count = len(signature(f).parameters)
+                args = self._parse_arguments(need_count)
+                if not args is None:
+                    f(*args)
             else:
                 print('command "{}" not found. available={}'.format(func, list(funcs.keys())))
 
