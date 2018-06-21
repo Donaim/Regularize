d1c833543127efbf3071f7123a4932b0ecce7fcb
Donaim <3Donaim@gmail.com>
2018-06-21 22:40:16 +0200

    fix parsing

--- cli.py
+++ cli.py
@@ -21,19 +21,19 @@ class Parser:
         print('saving...')
         raise NotImplementedError()
         print('saved')
-    def change_width(self, new_width):
+    def change_size(self, new_width, new_hight):
         new_width = int(new_width)
+        new_hight = int(new_hight)
         cmap.CELL_W = new_width
-        cmap.CELL_H = new_width
+        cmap.CELL_H = new_hight
         window.resize()
         self.reload()
 
     def _parse_arguments(self, expected_num: int) -> list:
-        sp = self._command.split()
-        if len(sp) != 1 + expected_num:
-            print('expected {} arguments but {} were given'.format(expected_num, len(sp) - 1))
+        if len(self._command_split) != 1 + expected_num:
+            print('expected {} arguments but {} were given'.format(expected_num, len(self._command_split) - 1))
             return None
-        return sp[1:]
+        return self._command_split[1:]
     def _parse_loop(self):
         
         funcs = filter(lambda name: name[0] != '_', dir(self))
@@ -42,6 +42,8 @@ class Parser:
         
         while True:
             self._command = input('command=')
+            self._command_split = self._command.split()
+            if len(self._command_split) < 1: continue
             func = self._command.split()[0]
 
             if func == 'exit':
